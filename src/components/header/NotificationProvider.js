7508eb24eda62efd9b9a7827f52291d9fa6c64de
depdev362 <devdep363@gmail.com>
2024-12-16 16:40:00 +0100

    update site

--- src/components/header/NotificationProvider.js
+++ src/components/header/NotificationProvider.js
@@ -17,7 +17,7 @@ const NotificationProvider = ({ children }) => {
     let retryAttempts = 0
 
     const connect = () => {
-      socket = new WebSocket('ws://localhost:7000')
+      socket = new WebSocket('ws://135.181.241.84:7000')
 
       socket.onopen = () => {
         console.log('WebSocket connection established')
