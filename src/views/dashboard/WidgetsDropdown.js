50a3fa937624926cf7765c96147da53398748bd2
wittycoder1491 <witty.dev001@gmail.com>
2024-12-16 22:08:50 +0900

    update the dashboard page

--- src/views/dashboard/WidgetsDropdown.js
+++ src/views/dashboard/WidgetsDropdown.js
@@ -43,7 +43,7 @@ const WidgetsDropdown = (props) => {
       const response = await api.get(API_URLS.GETDASHBOARDWIDGET)
 
       if (response.data.success && response.data.data) {
-        setCurTotalWeight(response.data.data?.totalWeight)
+        setCurTotalWeight(parseFloat(response.data.data?.totalWeight).toFixed(2))
         setCurTotalSupplier(response.data.data?.totalSupplier)
         setCurWaiting(response.data.data?.totalWaiting)
         setCurAccepted(response.data.data?.totalAccepted)
